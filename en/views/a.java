4a139befc0ce0d0dc844cd7462d7f8fe58029f34
Hongtao REN From Mac <h-t-ren@icloud.com>
2017-08-09 13:26:21 +0800

    first commit

--- en/views/a.java
+++
@@ -1,5 +0,0 @@
-@RequestParam(value = "types", required = false) List<Long> types
-@Transactional(readOnly = true)
-@RequestMapping(value = "/s/getMaterialListObjs", method = RequestMethod.GET)
-public List<WSSelectObj> getMaterialListObjs(
-@RequestParam(value = "q", required = false) String q,@RequestParam(value = "types", required = false) List<Long> types)
